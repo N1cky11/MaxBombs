19e5cf06ebd752d5d84ae9354297beda202f2b63
N1cky11 <129217979+N1cky11@users.noreply.github.com>
2023-12-26 20:39:31 +0400

    Add gtest and read function

--- BombSolverMain.cpp
+++ BombSolverMain.cpp
@@ -1,11 +1,43 @@
-#include "bombSolverClass.h"
+#include "BombSolverClass.h"
 #include <iostream>
 #include <vector>
+#include <gtest/gtest.h>
+#include <fstream>
+#include <sstream>
 
+TEST(BombSolver,vectors) {
+BombsSolver bombsSolver;
+ASSERT_EQ(2,bombsSolver.findMaxDetonations(std::vector<Point>{Point(2,1,3),Point(6,1,4)} ) ) ;
+ASSERT_EQ(1,bombsSolver.findMaxDetonations(std::vector<Point>{Point(1,1,5),Point(10,10,5)} ) ) ;
+ASSERT_EQ(5,bombsSolver.findMaxDetonations(std::vector<Point>{Point(1,2,3),Point(2,3,1),Point(3,4,2),Point(4,5,3),Point(5,6,4)} ) ) ;
 
-int main() {
-    std::vector<Point> points{Point(2,1,3), Point(6,1,4)};
+}
+void readPoints(std::istream &in, std::vector<Point> &vec) {
+    std::string line;
+    while (getline(in, line)) {
+        std::stringstream ss(line);
+        int x{}, y{}, r{};
+        while(ss>>x>>y>>r) {
+ vec.emplace_back(Point(x, y, r) ) ;
+}
+    }
+}
+
+int main(int argc, char **argv) {
+
+    std::vector<Point> points{} ;
+    std::ifstream in("InputFile.txt") ;
+    if(!in.is_open() ) {
+            std::cout <<"Failed to open file\n" ;
+            return 1 ;
+    }
+
+    ::testing::InitGoogleTest() ;
+    return RUN_ALL_TESTS() ;
+
+    readPoints(in,points) ;
+
     BombsSolver bombsSolver;
-    std::cout << bombsSolver.findMaxDetonations(points);
+    std::cout<< "\nMax number of detonated Bombs="<< bombsSolver.findMaxDetonations(points) << "\n";
     return 0;
 }
